0dbbb249ec7c547c1d2d5a578fc3eb486651b512
Filipe Costa <filipebarcos@gmail.com>
2012-12-20 22:48:09 -0300

    Adding a background image to it

--- rnake.rb
+++ rnake.rb
@@ -3,11 +3,13 @@ class Rnake < Gosu::Window
     super 640, 480, false
     self.caption = "RNAKE"
 
+    @background_image = Gosu::Image.new(self, 'media/images/background.jpg')
   end
 
   def update
   end
 
   def draw
+    @background_image.draw(0, 0, 0)
   end
 end
